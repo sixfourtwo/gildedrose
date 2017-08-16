699392cdb21e4610945529988f2fc0648a5ba207
Christoffer Junge <201305945@post.au.dk>
2017-08-16 15:07:01 +0200

    Comment commit

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -12,6 +12,7 @@ public class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
+	/// The comment
     }
 
 }
